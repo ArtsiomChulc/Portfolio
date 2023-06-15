befd186a0edd2908c36dac443207126916be4e1b
ArtsiomChulc <artemorsha@mail.ru>
2023-06-15 19:27:21 +0300

    fix login-form

--- src/components/login/LogIn.tsx
+++ src/components/login/LogIn.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import s from './LogIn.module.scss';
 import button from "../../common/styles/button.module.scss";
+import reactImg from '../../common/icons/iconForMyWorksExample/react.svg'
 
 const LogIn = () => {
     return (
         <div className={s.container}>
-            <div className={s.brand_logo}></div>
             <div className={s.brand_title}>Contacts form</div>
+            <div className={`${s.brand_logo} ${s.pulse}`}>
+                <img src={reactImg} alt="React logo"/>
+            </div>
             <div className={s.inputs}>
                 <label>Enter your Name</label>
                 <input className={s.input} type="text" placeholder="Name"/>
